21e1f4f6baab729ca7b1b5d0d7b127b6b350d608
jira-edu <jirapong.edu@gmail.com>
2024-07-28 17:18:28 +0700

    first commit

---
+++ extension.js
@@ -0,0 +1,13 @@
+({
+    name: "SPI-RFID", // Category Name
+    description: "ESP32 RFID reader Mifare 13.56MHz SPI interface",
+    author: "Jira-edu",
+    category: "Sensors",
+    version: "1.0.0",
+    icon: "/static/icon.png", // Category icon
+    color: "#3498DB", // Category color (recommend some blocks color)
+    blocks: [ // Blocks in Category
+        "rfid_is_detected",
+        "rfid_read_uid"
+    ]
+});
